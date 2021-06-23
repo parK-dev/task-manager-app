4da566cac78410b92d597b4f5dba7be7ad2d1034
Kirk Paradis <kirk.paradis@gmail.com>
2021-06-23 16:01:58 +0900

    Able to connect to the cluster and write to a collection

--- mongodb.js
+++ mongodb.js
@@ -5,12 +5,22 @@ const MongoClient = mongodb.MongoClient;
 
 const uri = `mongodb+srv://parkdev:${process.env.DB}@cluster0.f5zgd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const dbName = 'taskManagerDB';
+
+
+
 client.connect(err => {
   if (err) {
-    return console.log(err);
+    return console.error(err);
   };
   console.log('Connected successfully.');
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+  const task = {title: 'Figure it out', date: 'today'};
+  const collection = client.db(dbName).collection("Tasks");
+  collection.insertOne(task, (err, res) =>{
+    if (err) {
+      return console.error(err);
+    };
+    client.close();
+  }); 
 });
+
