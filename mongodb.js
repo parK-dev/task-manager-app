6622803ac38c8d01c9908eb93f545e693e6d825a
Kirk Paradis <kirk.paradis@gmail.com>
2021-06-23 16:20:31 +0900

    Refactored uri

--- mongodb.js
+++ mongodb.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv/config';
 
 const MongoClient = mongodb.MongoClient;
 
-const uri = `mongodb+srv://parkdev:${process.env.DB}@cluster0.f5zgd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = process.env.DB.toString();
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const dbName = 'taskManagerDB';
 
