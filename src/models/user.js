a6d8337570233891c63d5d76434d641de892e6f9
Kirk Paradis <kirk.paradis@gmail.com>
2021-06-27 14:41:30 +0900

    Added findbycredentials method for login purposes

--- src/models/user.js
+++ src/models/user.js
@@ -6,11 +6,13 @@ const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
+    unique: true,
     trim: true
   },
   email: {
     type: String,
     required: true,
+    unique: true,
     validate(value) {
       if (!validator.isEmail(value)) {
         throw new Error('The email is not valid');
@@ -29,6 +31,14 @@ const userSchema = new mongoose.Schema({
     }
   }
 });
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) { throw new Error('Unable to login')};
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {throw new Error('Unable to login')};
+  return user;
+};
 
 // Password Hashing
 userSchema.pre('save', async function (next) {
--- src/routers/user.js
+++ src/routers/user.js
@@ -31,7 +31,16 @@ router.post('/users', async (req, res) => {
     res.status(201).send(user);
   } catch (e) {
     res.status(400).send(e);
-  }
+  };
+});
+
+router.post('/users/login', async (req, res) => {
+  try {
+    const user = await User.findByCredentials(req.body.email, req.body.password);
+    res.send(user);
+  } catch (e) {
+    
+  };
 });
 
 router.patch('/users/:id', async(req, res) => {
