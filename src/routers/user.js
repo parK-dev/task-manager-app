ae67b6fddf7d8c5c7f59040ab5af079da871aee7
Kirk Paradis <kirk.paradis@gmail.com>
2021-06-28 20:17:41 +0900

    Removed useless route users/:id

--- src/routers/user.js
+++ src/routers/user.js
@@ -10,16 +10,6 @@ router.get('/users/me', auth, async (req, res) => {
   res.send(req.user);
 });
 
-router.get('/users/:id', auth, async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    if (!user) { return res.status(404).send() };
-    res.status(200).send(user);
-  } catch (e) {
-    res.status(500).send(e);
-  };
-});
-
 router.post('/users', async (req, res) => {
   try {
     const user = new User(req.body);
