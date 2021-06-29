7e7ea621f2ce8fd45bd1eb11c6dd19f816c942dd
Kirk Paradis <kirk.paradis@gmail.com>
2021-06-29 11:47:41 +0900

    File upload implementation for avatar

--- src/models/user.js
+++ src/models/user.js
@@ -36,7 +36,10 @@ const userSchema = new mongoose.Schema({
       type: String,
       required: true
     }
-  }]
+  }],
+  avatar: {
+    type: Buffer
+  }
 }, {
   timestamps: true
 });
@@ -45,6 +48,7 @@ userSchema.methods.toJSON = function () {
   const userObject = this.toObject();
   delete userObject.password;
   delete userObject.tokens;
+  delete userObject.avatar;
   return userObject;
 };
 
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,8 +1,20 @@
 import express from 'express';
 import User from '../models/user.js';
 import auth from '../middleware/auth.js';
+import multer from 'multer';
+import sharp from 'sharp';
 
 const router = new express.Router();
+const upload = multer({
+  limits: {
+    fileSize: 1000000
+  },
+  fileFilter(req, file, callback) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return callback(new Error('File must be an image'));
+    };
+  }
+});
 
 // User Routes
 
@@ -48,6 +60,36 @@ router.post ('/users/logoutAll', auth, async (req, res) => {
     req.user.tokens = [];
     await req.user.save();
     res.send();
+  } catch (e) {
+    res.status(500).send();
+  };
+});
+
+router.get('/users/:id/avatar', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user || !user.avatar) { throw new Error() };
+    res.set('Content-Type', 'image/png');
+    res.send(user.avatar);
+  } catch (e) {
+    res.status(404).send(e);
+  };
+});
+
+router.post ('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+  const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250}).png().toBuffer();
+  req.user.avatar = buffer;
+  await req.user.save();
+  res.status(200).send();
+}, (error, req, res, next) => {
+  res.status(400).send({ error: error.message });
+});
+
+router.delete ('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+  try {
+    req.user.avatar = undefined;
+    await req.user.save();
+    res.status(200).send();
   } catch (e) {
     res.status(500).send();
   };
